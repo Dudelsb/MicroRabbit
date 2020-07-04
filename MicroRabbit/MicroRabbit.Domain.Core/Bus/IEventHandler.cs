9978eda90e696a18434fbe4351a269b564cacd40
Lakhan <x2dzya@gmail.com>
2020-07-04 12:49:52 +0530

    Added Domain Classes

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+         where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessgaeType { get; protected set; }
+
+        protected Message()
+        {
+            MessgaeType = GetType().Name;
+        }
+    }
+}
