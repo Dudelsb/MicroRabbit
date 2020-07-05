7729e1f3ba892a1d42fefc55c3124a95c3533f5a
Lakhan <x2dzya@gmail.com>
2020-07-05 17:14:34 +0530

    transfer controller added

---
+++ MicroRabbit/MicroRabbit.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,32 @@
+﻿using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : ControllerBase
+    {
+       
+
+        private readonly ILogger<TransferController> _logger;
+        private readonly ITransferService _transferService;
+
+        public TransferController(ILogger<TransferController> logger, ITransferService transferService)
+        {
+            _logger = logger;
+            _transferService = transferService;
+        }
+
+        [HttpGet]
+        public ActionResult< IEnumerable<TransferLog>> Get()
+        {
+            return Ok(_transferService.GetTransferLogs());
+        }
+    }
+}
