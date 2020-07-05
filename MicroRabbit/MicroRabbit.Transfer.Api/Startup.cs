b0ed68389a50d6adeaacab7ce4b8c7daff7eab3c
Lakhan <x2dzya@gmail.com>
2020-07-05 13:49:22 +0530

    added projects to transfer microservices

--- MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Infra.IOC;
+using MicroRabbit.Transfer.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,9 +29,25 @@ namespace MicroRabbit.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TransferDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BankingDBConnection"));
+            });
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking MicroService", Version = "v1" });
+            });
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
         }
 
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -41,6 +61,10 @@ namespace MicroRabbit.Transfer.Api
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer MicroService v1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
